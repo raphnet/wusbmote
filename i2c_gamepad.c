ac81340dc0f988d238c3989a8ae3ae3a8b437037
Raphaël Assénat <raph@raphnet.net>
2012-09-07 02:44:11 +0000

    Auto detects the controller type. Tested and works with:  - Nunchuck  - Classic controller RVL-005  - Black Classic controller RVL-005(-02)

--- i2c_gamepad.c
+++ i2c_gamepad.c
@@ -27,7 +27,6 @@
 #include "i2c.h"
 
 #define REPORT_SIZE		8
-
 #define I2C_ADDRESS		0x52
 
 // report matching the most recent bytes from the controller
@@ -39,11 +38,22 @@ static unsigned char last_reported_controller_bytes[REPORT_SIZE];
 #define DEBUGLOW()		PORTC &= 0xFE
 #define DEBUGHIGH()		PORTC |= 0x01
 
+#define W2I_REG_REPORT		0x00
+#define W2I_REG_UNKNOWN_F0	0xF0
+#define W2I_REG_UNKNOWN_FB	0xFB
+#define W2I_REG_ID_L		0xFE
+#define W2I_REG_ID_H		0xFF
+
 
 #define STATE_INIT		0
 #define STATE_READ_DATA	1
 
 static char state = STATE_INIT;
+
+#define ID_NUNCHUCK	0x0000
+#define ID_CLASSIC	0x0101
+
+static unsigned short peripheral_id = ID_NUNCHUCK;
 
 static void pulseres(int res)
 {
@@ -60,71 +70,161 @@ static void pulseres(int res)
 	}
 }
 
+static char w2i_reg_writeByte(unsigned char reg_addr, unsigned char value)
+{
+	unsigned char tmpbuf[2];
+	char res;
+
+	tmpbuf[0] = reg_addr;
+	tmpbuf[1] = value;
+
+	res = i2c_transaction(I2C_ADDRESS, 2, tmpbuf, 0, NULL, 0);
+	_delay_us(400);
+
+	return res;
+}
+
+static char w2i_reg_readBlock(unsigned char reg_addr, unsigned char *dst, int len)
+{
+	char res;
+
+	res = i2c_transaction(I2C_ADDRESS, 1, &reg_addr, 0, NULL, 0);
+	if (res)
+		return res;
+
+	_delay_us(400);
+
+	res = i2c_transaction(I2C_ADDRESS, 0, NULL, len, dst, 0);
+	if (res)
+		return res;
+
+	_delay_us(400);
+
+	return 0;
+}
+
+static void setLastValues(unsigned char x, unsigned char y, unsigned short rx, unsigned short ry, unsigned short rz, unsigned char btns_l, unsigned char btns_h)
+{
+	last_read_controller_bytes[0] = x;
+	last_read_controller_bytes[1] = y;
+	last_read_controller_bytes[2] = rx;
+	last_read_controller_bytes[3] = rx >> 8;
+	last_read_controller_bytes[3] |= ry << 2;
+	last_read_controller_bytes[4] = ry >> 6;
+	last_read_controller_bytes[4] |= rz << 4;
+	last_read_controller_bytes[5] = rz >> 4;
+	last_read_controller_bytes[6] = btns_l;
+	last_read_controller_bytes[7] = btns_h;
+}
+
 static void i2cGamepad_Update(void)
 {
 	unsigned char buf[16];
 	char res;
-	int i;
+	unsigned char x=0x80,y=0x80;
+	unsigned char btns_l=0, btns_h=0;
+	unsigned short rx=0x200,ry=0x200,rz=0x200;
 
 	switch (state)
 	{
 
 		case STATE_INIT:
-			buf[0] = 0xF0; // Accessory register address
-			buf[1] = 0x55; // Value
-			
-			res = i2c_transaction(I2C_ADDRESS, 2, buf, 0, NULL, 0);
+			//
+			// Init sequence from:
+			//
+			// http://wiibrew.org/wiki/Wiimote/Extension_Controllers
+			//	
+			res = w2i_reg_writeByte(W2I_REG_UNKNOWN_F0, 0x55);
 			if (res)
+				return;
+			
+			res = w2i_reg_writeByte(W2I_REG_UNKNOWN_FB, 0x00);
+			if (res)		
 				return;
 		
-			_delay_us(100);
-			_delay_ms(10);
-			
-			buf[0] = 0xFB; // Accessory register address
-			buf[1] = 0x00; // Value
-			res = i2c_transaction(I2C_ADDRESS, 2, buf, 0, NULL, 0);
+			res = w2i_reg_readBlock(W2I_REG_ID_L, buf, 2);
 			if (res)
-				return;
+				return;	
 
+			peripheral_id = buf[0] | buf[1]<<8;
+			
 			state = STATE_READ_DATA;
 			_delay_ms(1000);
 			break;
 
 			// fallthrough
 		case STATE_READ_DATA:
-			buf[0] = 0x00;
-			res = i2c_transaction(I2C_ADDRESS, 1, buf, 0, NULL, 0);
+			res = w2i_reg_readBlock(W2I_REG_REPORT, buf, 6);
 			if (res) {
 				state = STATE_INIT;
 				return;
 			}
-			_delay_us(400);
+		
+			switch (peripheral_id)
+			{
+				default:
+				case ID_NUNCHUCK:
+					// Source: http://wiibrew.org/wiki/Wiimote/Extension_Controllers/Nunchuck
+					//
+					//     7   6    5   4    3   2     1   0
+					// 0   SX<7:0>
+					// 1   SY<7:0>
+					// 2   AX<9:2>
+					// 3   AY<9:2>
+					// 4   AZ<9:2>
+					// 5   AZ<1:0>  AY<1:0>  AX<1:0>   BC  BZ
+					//
+					x = buf[0];
+					y = buf[1] ^ 0xff;
+					rx = ((buf[5] & 0x0C) >> 2)	| (buf[2] << 2);
+					ry = ((buf[5] & 0x30) >> 4)	| (buf[3] << 2);
+					rz = ((buf[5] & 0xC0) >> 6)	| (buf[4] << 2);
+					
+					if (!(buf[5]&0x01)) btns_l |= 0x01;
+					if (!(buf[5]&0x02)) btns_l |= 0x02;
+					break;
+
+				case ID_CLASSIC:
+					// Source: http://wiibrew.org/wiki/Wiimote/Extension_Controllers/Classic_Controller
+					// 
+					//     7        6     5    4    3     2     1     0 
+					// 0   RX<4:3>        LX<5:0>
+					// 1   RX<2:1>        LY<5:0>
+					// 2   RX<0>    LT<4:3>    RY<4:0>
+					// 3   LT<2:0>             RT<4:0>
+					// 4   BDR      BDD   BLT  B-   BH    B+    BRT   1 
+					// 5   BZL      BB    BY   BA   BX    BZR   BDL   BDU
+					//
+					x = buf[0] << 2;
+					y = (buf[1] << 2) ^ 0xFF;
+					rx = ((buf[2]>>7) | ((buf[1]&0xC0)>>5) | ((buf[0]&0xC0)>>3)) << 5;
+					ry = ((buf[2]&0x1f) << 5) ^ 0x3FF;
+					rz = ((buf[3]>>5) | ((buf[2] & 0x60) >> 2) ) << 5;
 			
-			res = i2c_transaction(I2C_ADDRESS, 0, NULL, 6, buf, 0);
-			_delay_us(400);
+					// The fist 12 USB button IDs follow the assignments of my Gamecube to USB adapter project.
+					if (!(buf[4] & 0x04)) btns_l |= 0x01; // +/Start
+					if (!(buf[5] & 0x20)) btns_l |= 0x02; // Y
+					if (!(buf[5] & 0x08)) btns_l |= 0x04; // X
+					if (!(buf[5] & 0x40)) btns_l |= 0x08; // B
+					if (!(buf[5] & 0x10)) btns_l |= 0x10; // A
+					if (!(buf[4] & 0x20)) btns_l |= 0x20; // L trig
+					if (!(buf[4] & 0x02)) btns_l |= 0x40; // R trig
+					if (!(buf[5] & 0x04)) btns_l |= 0x80; // Zr
+					if (!(buf[5] & 0x01)) btns_h |= 0x01; // UP
+					if (!(buf[4] & 0x40)) btns_h |= 0x02; // DOWN
+					if (!(buf[4] & 0x80)) btns_h |= 0x04; // RIGHT
+					if (!(buf[5] & 0x02)) btns_h |= 0x08; // LEFT
 
-			last_read_controller_bytes[0] = buf[0];
-			last_read_controller_bytes[1] = buf[1] ^ 0xff;
+					if (!(buf[5] & 0x80)) btns_h |= 0x10; // Zl
+					if (!(buf[4] & 0x10)) btns_h |= 0x20; // SELECT
+					if (!(buf[4] & 0x08)) btns_h |= 0x40; // HOME
+					break;
 
-
-			last_read_controller_bytes[2] = (buf[5] & 0x0C) >> 2;
-			last_read_controller_bytes[2] |= buf[2] << 2;
-			last_read_controller_bytes[3] = buf[2] >> 6;
-
-			last_read_controller_bytes[3] |= (buf[5] & 0x30) << 2;
-			last_read_controller_bytes[3] |= (buf[3] << 4);
-			last_read_controller_bytes[4] = buf[3] >> 4;
-
-			last_read_controller_bytes[4] |= (buf[5] & 0xC0) << 4;
-			last_read_controller_bytes[4] |= buf[4] << 6;
-			last_read_controller_bytes[5] = buf[4] >> 2;
-
-			
-			last_read_controller_bytes[6] = (buf[5] & 0x03) ^ 0x03;
-
+			}
 			break;
 	}
 
+	setLastValues(x,y,rx,ry,rz,btns_l,btns_h);
 }	
 
 static void i2cGamepad_Init(void)
